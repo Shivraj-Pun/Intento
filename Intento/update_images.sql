d193a69ef442a85a28d6e2553b81148347f41c65
Shivraj Pun <shivrajpun8305@gmail.com>
2026-07-19 02:37:55 +0530

    Remove old ProductDTO and add update_images.sql

---
+++ Intento/update_images.sql
@@ -0,0 +1,29 @@
+-- Auto-generated SQL to update product images
+BEGIN;
+
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Stack_of_egg_cartons.jpg/500px-Stack_of_egg_cartons.jpg' WHERE sku = 'PARTY-SPOON-50PC';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Hip%2C_Hip%2C_Hurrah%21_Artists%E2%80%99_Party%2C_Skagen_%28Peder_Severin_Kr%C3%B8yer%29_-_Gothenburg_Museum_of_Art_-_F_62.tif/lossy-page1-500px-Hip%2C_Hip%2C_Hurrah%21_Artists%E2%80%99_Party%2C_Skagen_%28Peder_Severin_Kr%C3%B8yer%29_-_Gothenburg_Museum_of_Art_-_F_62.tif.jpg' WHERE sku = 'PARTY-HAT-10PC';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Crepes_dsc07085.jpg/500px-Crepes_dsc07085.jpg' WHERE sku = 'FIRSTAID-BANDAGE-1PC';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Male_and_female_chicken_sitting_together.jpg/500px-Male_and_female_chicken_sitting_together.jpg' WHERE sku = 'MEAT-CHICKEN-CURRYCUT-1KG';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/The_newborn_baby_in_men.jpg/500px-The_newborn_baby_in_men.jpg' WHERE sku = 'BABY-LOTION-200ML';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/%C5%A0v%C3%A9dsk%C3%BD_kol%C3%A1%C4%8D_naruby_904_%28cropped%29.JPG/500px-%C5%A0v%C3%A9dsk%C3%BD_kol%C3%A1%C4%8D_naruby_904_%28cropped%29.JPG' WHERE sku = 'PANTRY-BUTTER-CHICKEN-MASALA-50G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/95_105_Scotch_Brite_%28155385643%29.jpeg/500px-95_105_Scotch_Brite_%28155385643%29.jpeg' WHERE sku = 'CLEAN-SCRUB-3PC';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Mangos_-_single_and_halved.jpg/500px-Mangos_-_single_and_halved.jpg' WHERE sku = 'BEV-LASSI-200ML';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Solanum_melongena_24_08_2012_%281%29.JPG/500px-Solanum_melongena_24_08_2012_%281%29.JPG' WHERE sku = 'PRODUCE-BRINJAL-500G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/%22Hyderabadi_Dum_Biryani%22.jpg/500px-%22Hyderabadi_Dum_Biryani%22.jpg' WHERE sku = 'PANTRY-BIRYANI-MASALA-50G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Rohu_at_Giant_Hypermarket_Kota_Damansara_20230203_105829.jpg/500px-Rohu_at_Giant_Hypermarket_Kota_Damansara_20230203_105829.jpg' WHERE sku = 'MEAT-FISH-ROHU-500G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Macaroni2.jpg/500px-Macaroni2.jpg' WHERE sku = 'PANTRY-MACARONI-500G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Rasam.JPG/500px-Rasam.JPG' WHERE sku = 'PANTRY-RASAM-POWDER-100G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/P1030323.JPG/500px-P1030323.JPG' WHERE sku = 'PRODUCE-LEMON-4PC';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Runny_hunny.jpg/500px-Runny_hunny.jpg' WHERE sku = 'PANTRY-HONEY-500G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Male_and_female_chicken_sitting_together.jpg/500px-Male_and_female_chicken_sitting_together.jpg' WHERE sku = 'MEAT-CHICKEN-BONELESS-500G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Mozzarella_di_bufala3.jpg/500px-Mozzarella_di_bufala3.jpg' WHERE sku = 'DAIRY-MOZZARELLA-200G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Plasma_globe_60th.jpg/500px-Plasma_globe_60th.jpg' WHERE sku = 'BEV-ENERGY-250ML';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/8/8d/ChineseDishLogo.png' WHERE sku = 'PANTRY-SCHEZWAN-SAUCE-250G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Cashew_apples.jpg/500px-Cashew_apples.jpg' WHERE sku = 'PANTRY-CASHEW-100G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Male_and_female_chicken_sitting_together.jpg/500px-Male_and_female_chicken_sitting_together.jpg' WHERE sku = 'MEAT-CHICKEN-WINGS-500G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Aamsestraat_1_Blokvormig_wit_gepleisterd_dubbel_woonhuis.jpg/500px-Aamsestraat_1_Blokvormig_wit_gepleisterd_dubbel_woonhuis.jpg' WHERE sku = 'PANTRY-PICKLE-MANGO-400G';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Church_Farm%2C_Stockton%2C_Norfolk_-_geograph.org.uk_-_1968544.jpg/500px-Church_Farm%2C_Stockton%2C_Norfolk_-_geograph.org.uk_-_1968544.jpg' WHERE sku = 'DAIRY-EGGS-6PC';
+UPDATE public.products SET image_name = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Illustration_Trigonella_foenum-graecum0_clean.jpg/500px-Illustration_Trigonella_foenum-graecum0_clean.jpg' WHERE sku = 'PRODUCE-METHI-1BUNCH';
+
+COMMIT;
